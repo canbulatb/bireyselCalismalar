91de800905f48d68143cd72ff2607f4774a17be6
canbulatb <canbulattb@gmail.com>
2023-03-14 23:34:04 +0300

    a
    a

---
+++ ifCalisma2.py
@@ -0,0 +1,7 @@
+yas=int(input("Lütfen yaşınızı giriniz...: "))
+if yas>18:
+    print("Eh yaş artık kemale eriyor yavaş yavaş.")
+elif yas<0:
+    print("Yok artık daha neler.")    
+elif yas<=18:
+    print("Genç bir kardeşimizsiniz")
---
+++ ifÇalisma.py
@@ -0,0 +1,8 @@
+sayi=int(input("Bir sayı giriniz...: "))
+
+if sayi>10:
+    print("Sayı 10'dan büyüktür")
+elif sayi<10:
+    print("Sayı 10'dan küçüktür")
+else:
+    print("Sayı 10'a eşittir.")
