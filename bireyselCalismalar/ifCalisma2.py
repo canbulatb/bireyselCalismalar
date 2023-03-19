b7d0a881f643ecab84fe65f9119505ba58ce5ff2
canbulatb <canbulattb@gmail.com>
2023-03-20 00:20:50 +0300

    2. hafta ödevler
    2. hafta ödevler.

--- bireyselCalismalar/ifCalisma2.py
+++
@@ -1,7 +0,0 @@
-yas=int(input("Lütfen yaşınızı giriniz...: "))
-if yas>18:
-    print("Eh yaş artık kemale eriyor yavaş yavaş.")
-elif yas<0:
-    print("Yok artık daha neler.")    
-elif yas<=18:
-    print("Genç bir kardeşimizsiniz")
--- bireyselCalismalar/ifÇalisma.py
+++
@@ -1,8 +0,0 @@
-sayi=int(input("Bir sayı giriniz...: "))
-
-if sayi>10:
-    print("Sayı 10'dan büyüktür")
-elif sayi<10:
-    print("Sayı 10'dan küçüktür")
-else:
-    print("Sayı 10'a eşittir.")
