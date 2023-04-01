0dc71305f6292389df711bc4d362af8de4060733
canbulatb <canbulattb@gmail.com>
2023-04-01 12:01:06 +0300

    .
    .

--- listCalisma.py
+++ listCalisma.py
@@ -1,3 +1,12 @@
 sayilar = [[0, 10], [6, 60], [12, 54], [67, 99]]
 for i in sayilar:
-    print(*range(*i))
+    print(*range(*i)) #* ne işe yarıyordu...
+
+
+alfabe = "abcçdefgğhıijklmnoöprsştuüvyz"
+harfListesi=list(alfabe)
+print(harfListesi)
+
+isimler = "elma, armut, çilek"
+isim=isimler.split(", ")
+print(isim)
